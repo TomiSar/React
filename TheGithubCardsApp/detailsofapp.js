1bd4ba59c8484e77a753bd7d9d32fa666a881b89
Tomi Sarjamo <tomi.sarjamo@tieto.com>
2019-12-13 15:31:45 +0200

    Started the new part of the module

---
+++ TheGithubCardsApp/detailsofapp.js
@@ -0,0 +1,18 @@
+//The Github cards app
+
+//What we are building
+//url: https://jscomplete.com/playground/rgs2.1
+//React class components
+//url: https://jscomplete.com/playground/rgs2.1
+//Styling React components
+//url: https://jscomplete.com/playground/rgs2.2
+//Working with data
+//url: https://jscomplete.com/playground/rgs2.3
+//Iniializing ans Reading the statement from data
+//url: https://jscomplete.com/playground/rgs2.4
+//Taking input from the User
+//url: https://jscomplete.com/playground/rgs2.5
+//Working with Ajax calls
+//url: https://jscomplete.com/playground/rgs2.6
+//Wrap up
+//url: https://jscomplete.com/playground/rgs2.7
